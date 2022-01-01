1e1a3a42ca67dc2d337171c0a04495bf687fd68a
Koramit Pichanaharee <koramit.p@gmail.com>
2022-01-01 21:00:48 +0700

    init login

---
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\APIs\SubHannahAPI;
+use App\Contracts\AuthenticationAPI;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Str;
+use Inertia\Inertia;
+
+class AuthenticatedSessionController extends Controller
+{
+    /**
+     * Display the login view.
+     *
+     * @return \Inertia\Respons
+     */
+    public function create()
+    {
+        return Inertia::render('Auth/Login');
+    }
+
+    public function store()
+    {
+        Request::validate([
+            'login' => 'required|string',
+            'password' => 'required|string',
+        ]);
+
+        // if (config('auth.guards.web.provider') === 'avatars') {
+        //     return $this->storeAvatarUser();
+        // }
+
+        $api = new SubHannahAPI;
+
+        $data = $api->authenticate(Request::input('login'), Request::input('password'));
+
+        if (! $data['found']) {
+            return back()->withErrors([
+                'login' => $data['message'],
+            ]);
+        }
+
+        if (! $user = User::whereLogin(Request::input('login'))->first()) {
+            $user = $this->autoRegister($data);
+        }
+
+        Auth::login($user);
+
+        return Redirect::intended(route($user->home_page));
+
+
+
+        // Session::put('profile', $data);
+
+        // return Redirect::route('register');
+    }
+
+    public function update()
+    {
+        return ['active' => true];
+    }
+
+    public function destroy()
+    {
+        Auth::guard('web')->logout();
+
+        Request::session()->invalidate();
+
+        Request::session()->regenerateToken();
+
+        return Redirect::route('login');
+    }
+
+    // protected function storeAvatarUser()
+    // {
+    //     if (Auth::attempt(Request::only(['login', 'password']), )) {
+    //         return Redirect::intended(route(Auth::user()->home_page));
+    //     }
+
+    //     return back()->withErrors([
+    //         'login' => __('auth.failed'),
+    //     ]);
+    // }
+
+    protected function autoRegister($data)
+    {
+        $user =  User::create([
+            'name' => $data['username'],
+            'login' => $data['username'],
+            'full_name' => $data['name'],
+            'password' => Hash::make(Str::random(64)),
+        ]);
+
+        // \Log::info($user);
+
+        return User::find($user->id);
+    }
+}
---
+++ resources/js/Pages/Auth/Login.vue
@@ -0,0 +1,30 @@
+<template>
+    <h1>login</h1>
+    <div>
+        <input
+            type="text"
+            placeholder="login"
+            v-model="form.login"
+        >
+    </div>
+    <small class="text-red-400 text-sm">{{ form.errors.login }}</small>
+    <div>
+        <input
+            type="password"
+            placeholder="password"
+            v-model="form.password"
+        >
+    </div>
+    <button @click="form.post('/login')">
+        login
+    </button>
+</template>
+
+<script setup>
+import { useForm } from '@inertiajs/inertia-vue3';
+
+const form = useForm({
+    login: null,
+    password: null,
+});
+</script>
